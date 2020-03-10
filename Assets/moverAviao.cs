341cc265116b4ba0b198458272baef3a6acf5a8a
MISHELLE SOUSA SILVA <u18289@academico.cotuca.unicamp.br>
2020-03-10 15:12:10 -0300

    Jogo Avião

---
+++ Assets/moverAviao.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class moverAviao : MonoBehaviour
+{
+
+    public GameObject prefabTiroAviao;
+    public AudioClip somDeExplosao;
+    public float velocidadeTiroAviao = 4f;
+    public float taxaDeTiros = 0.05f;
+    private float proximoTiro = 0.0f;
+    public float velocidadeDoAviao = 3.0f;
+    public float taxaSpawnInimigos = 2;
+    public float xMaximo = 4, xMinimo = -4;
+    public float taxaSpawnAtual;
+    public int maxInimigos = 10;
+    public GameObject prefabInimigo;
+
+    void Start()
+    {
+        StaticGameController.listaInimigo = new List<GameObject>();
+        StaticGameController.ListaTiroInimigo = new List<GameObject>();
+        StaticGameController.listaTiroAviao = new List<GameObject>();
+        StaticGameController.criarListaInimigos(prefabInimigo, maxInimigos);
+
+    }
+
+    void verificaCriacaoDeInimigos()
+    {
+        taxaSpawnAtual += Time.deltaTime;
+        if (taxaSpawnAtual > taxaSpawnInimigos)
+        {
+            taxaSpawnAtual = 0;
+            StaticGameController.SpawnInimigos(xMinimo, xMaximo);
+        }
+    }
+    void TratarSetasDeDirecao(GameObject aviao)
+    {
+        float eixoHorizontal = Input.GetAxis("Horizontal");
+        float eixoVertical = Input.GetAxis("Vertical");
+        aviao.transform.position =
+            new Vector3(aviao.transform.position.x +
+            eixoHorizontal * velocidadeDoAviao * Time.deltaTime, aviao.transform.position.y +
+            eixoVertical * velocidadeDoAviao * Time.deltaTime,
+            aviao.transform.position.z);
+
+        if (eixoHorizontal > 0)
+        {
+            if (aviao.transform.position.x > 3.9f)
+                aviao.transform.position = new Vector3(3.9f, aviao.transform.position.y,
+                    aviao.transform.position.z);
+        }
+
+        else
+            if (eixoHorizontal < 0)
+            if (aviao.transform.position.x < -3.9f)
+                aviao.transform.position = new Vector3(-3.9f, aviao.transform.position.y,
+                    aviao.transform.position.z);
+
+        if (eixoVertical > 0)
+        {
+            if (aviao.transform.position.y > 6.5f)
+                SceneManager.LoadScene("cenaGanhou");
+
+        }
+
+        else
+           if (eixoVertical < 0)
+            if (aviao.transform.position.y < -3.9f)
+                aviao.transform.position = new Vector3(aviao.transform.position.x, -4.0f,
+                    aviao.transform.position.y);
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+#if UNITY_ANDROID || UNITY_WP8
+        if (Input.GetKeyDown(KeyCode.Escape))
+        Application.Quit();
+#endif
+
+        if (gameObject.tag == "Player")
+        {
+            verificaCriacaoDeInimigos();
+            Vector3 aceleracao = Input.acceleration;
+            if (Mathf.Abs(aceleracao.x) > 0.5f)
+            {
+                Vector3 direcao = Vector3.zero;
+                direcao.x = aceleracao.x;
+                direcao.y = aceleracao.y;
+                transform.Translate(direcao * velocidadeDoAviao * Time.deltaTime);
+                if (transform.position.x > 3.9f)
+                    transform.position = new Vector3(3.9f, transform.position.y, transform.position.z);
+                else
+                    if (transform.position.x < -3.9f)
+                    transform.position = new Vector3(3.9f, transform.position.y, transform.position.z);
+
+                if (transform.position.y > 6.5f)
+                    SceneManager.LoadScene("cenaGanhou");
+                else
+                    if (transform.position.y < -3.9f)
+                    transform.position = new Vector3(transform.position.x, -3.9f, transform.position.z);
+            }
+
+
+            TratarSetasDeDirecao(gameObject);
+            if (Input.GetKey(KeyCode.Space))
+                Debug.Log("Aqui criaremos um tiro do avião");
+
+            if (Input.touchCount == 1)
+            {
+                Touch toque = Input.GetTouch(0);
+                if (toque.phase == TouchPhase.Began)
+                    Debug.Log("Criar tiro por touch");
+            }
+
+        }
+
+        StaticGameController.moverTiros(3f);
+    }
+}
+
+
---
+++ Assets/scripts/StaticGameController.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StaticGameController {
+
+    public static List<GameObject> listaTiroAviao,
+        listaInimigo,
+        ListaTiroInimigo;
+
+    public static int UltimoTiroAviao = 0,
+        UltimoTiroInimigo = 0;
+
+
+    public static void criarTiro(GameObject aviao,GameObject prefabTiroAviao)
+    {
+        bool podeCriarSemRepeticao = true;
+        foreach (GameObject tiro in listaTiroAviao)
+        {
+            if(tiro.transform.position == aviao.transform.position)
+            {
+                podeCriarSemRepeticao = false;
+                break;
+            }
+        }
+
+        if (podeCriarSemRepeticao)
+        {
+            UltimoTiroAviao++;
+            GameObject tiroDoAviao = GameObject.Instantiate(prefabTiroAviao) as GameObject;
+
+            tiroDoAviao.name = "tiro" + StaticGameController.UltimoTiroAviao;
+
+            tiroDoAviao.transform.position = new Vector3(aviao.transform.position.x + 0.5f,
+                aviao.transform.position.y,
+                aviao.transform.position.z);
+
+            tiroDoAviao.SetActive(true);
+            ListaTiroInimigo.Add(tiroDoAviao);
+
+        }
+
+    }
+
+    public static void moverTiros (float velocidade)
+    {
+        foreach (GameObject tiro in listaTiroAviao)
+            tiro.transform.position = new Vector3(tiro.transform.position.x,
+               tiro.transform.position.y + velocidade * Time.deltaTime,
+               tiro.transform.position.z);
+
+        for (int i = listaTiroAviao.Count - 1; i>=0; i--)
+        {
+            GameObject tiro = listaTiroAviao[i];
+            if (tiro.transform.position.y > 8.5f)
+            {
+                listaTiroAviao[i].SetActive(false);
+                tiro.SetActive(false);
+                tiro.GetComponent<Renderer>().enabled = false;
+                listaTiroAviao.RemoveAt(i);            }
+        }
+    }
+
+    public static void removerTiros(GameObject qualTiro)
+    {
+        for (int i= listaTiroAviao.Count-1; i>=0; i--)
+        {
+            if (listaTiroAviao[i].name == qualTiro.name)
+            {
+                listaTiroAviao[i].SetActive(false);
+                qualTiro.SetActive(false);
+                qualTiro.GetComponent<Renderer>().enabled = false;
+                listaTiroAviao.RemoveAt(i);
+                break;
+            }
+        }
+    }
+
+    public static int MaxInimigos = 10;
+
+    public static void criarTiroInimigo(GameObject inimigo,
+        GameObject prefabTiroInimigo)
+    {
+        if(prefabTiroInimigo != null)
+        {
+            UltimoTiroInimigo++;
+            GameObject tiroDoInimigo = GameObject.Instantiate(prefabTiroInimigo)
+                as GameObject;
+            tiroDoInimigo.name = "tiroInimigo" +
+                StaticGameController.UltimoTiroInimigo;
+            tiroDoInimigo.transform.position = new Vector3(inimigo.transform.position.x,
+                inimigo.transform.position.y - 2,
+                inimigo.transform.position.z
+                );
+
+            tiroDoInimigo.SetActive(true);
+            ListaTiroInimigo.Add(tiroDoInimigo);
+        }
+    }
+
+    public static void moverTiroInimigos(float velocidade)
+    {
+        foreach (GameObject tiro in ListaTiroInimigo)
+        {
+            tiro.transform.position = new Vector3(tiro.transform.position.x,
+                tiro.transform.position.y + velocidade * Time.deltaTime,
+                tiro.transform.position.z);
+        }
+
+        for (int i = ListaTiroInimigo.Count - 1; i >= 0; i--)
+        {
+            GameObject qualTiro = ListaTiroInimigo[i];
+            if(qualTiro.transform.position.y < -4.5f)
+            {
+                ListaTiroInimigo[i].SetActive(false);
+                qualTiro.SetActive(qualTiro);
+                qualTiro.GetComponent<Renderer>().enabled = false;
+                ListaTiroInimigo.RemoveAt(i);
+            }
+        }
+    }
+
+    public static void removerTiroInimigo(GameObject qualTiro)
+    {
+        for (int i = ListaTiroInimigo.Count -1; i>=0; i--)
+        
+            if (ListaTiroInimigo[i].name == qualTiro.name)
+            {
+                Debug.Log("Removendo tiro inimigo" + qualTiro.name);
+                ListaTiroInimigo[i].SetActive(false);
+                qualTiro.SetActive(qualTiro);
+                qualTiro.GetComponent<Renderer>().enabled = false;
+                ListaTiroInimigo.RemoveAt(i);
+            }
+        
+    }
+    public static void InimigoSaiDeCena(GameObject outro)
+    {
+        for (int i = 0; i < MaxInimigos; i++)
+
+            if (listaInimigo[i].name == outro.name)
+            {
+                Debug.Log("Removendo tiro inimigo" + outro.name);
+                ListaTiroInimigo[i].SetActive(false);
+                outro.SetActive(outro);
+                outro.GetComponent<Renderer>().enabled = false;
+                listaInimigo.RemoveAt(i);
+                break;
+            }
+
+    }
+
+    public static void SpawnInimigos(float xMinimo, float xMaximo)
+    {
+        float posicaoX = 0;
+        GameObject inimigo = null;
+        for (int i= 0; i < MaxInimigos; i++)
+            if(listaInimigo[i].activeSelf == false)
+            {
+                inimigo = StaticGameController.listaInimigo[i];
+                posicaoX = Random.Range(xMinimo, xMaximo);
+                inimigo.transform.position = new Vector3(posicaoX, 10f, 1f);
+                inimigo.SetActive(true);
+                inimigo.GetComponent<Renderer>().enabled = true;
+                StaticGameController.listaInimigo[i] = inimigo;
+                break;
+            }
+    }
+
+    public static void criarListaInimigos(GameObject prefabInimigo, int maxInimigos)
+    {
+        MaxInimigos = maxInimigos;
+        listaInimigo = new List<GameObject>();
+        for (int i =0; i < maxInimigos; i++)
+        {
+            GameObject inimigo = GameObject.Instantiate(prefabInimigo) as GameObject;
+            inimigo.name = "inimigo" + i;
+            inimigo.SetActive(false);
+            inimigo.GetComponent<Renderer>().enabled = true;
+            listaInimigo.Add(inimigo);
+        }
+    }
+
+    public static void desativarInimigo (GameObject outro)
+    {
+        for (int i=0; i < MaxInimigos; i++)
+        {
+            if(listaInimigo[i].name == outro.name)
+            {
+                Debug.Log("inimigo desativado por colisão" + outro.name);
+                listaInimigo[i].SetActive(false);
+                outro.gameObject.SetActive(false);
+                listaInimigo[i].GetComponent<Renderer>().enabled = false;
+                break;
+            }
+        }
+    }
+
+
+}
